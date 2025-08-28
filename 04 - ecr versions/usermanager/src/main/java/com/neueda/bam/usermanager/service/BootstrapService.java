aced4b35953112a71d20a7fc406a90f34c2c8f40
Matt <matt@multicode.co.uk>
2025-08-28 16:11:43 +0100

    ecr version of usermanager

---
+++ 04 - ecr versions/usermanager/src/main/java/com/neueda/bam/usermanager/service/BootstrapService.java
@@ -0,0 +1,44 @@
+package com.neueda.bam.usermanager.service;
+
+import com.neueda.bam.usermanager.data.UserRepository;
+import com.neueda.bam.usermanager.domain.HRUser;
+import com.neueda.bam.usermanager.domain.HRUserList;
+import com.neueda.bam.usermanager.domain.User;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.util.List;
+
+@Service
+public class BootstrapService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${app.rest.hr.url}")
+    String hrUrl;
+
+
+    @PostConstruct
+    public void initData() {
+        if (userRepository.count() == 0) {
+            HRUserList list = restTemplate.getForObject(hrUrl + "&results=5000", HRUserList.class);
+            List<User> users = list.getResults().stream().map(HRUser::toUser).toList();
+            for (User user : users) {
+                try {
+                    userRepository.save(user);
+                }
+                catch (Exception e) {
+                    System.out.println("Unable to save " + user);
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
+    }
+}
