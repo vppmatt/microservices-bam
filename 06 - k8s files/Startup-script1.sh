605b867829283a69880e4d2e37ab579e506c3548
Matt Thornfield <matt@multicode.co.uk>
2025-11-20 10:58:38 +0000

    Updating startup scripts for kind
    Added installation steps for Kind, kubectl, and a local Docker registry. Configured a Kubernetes cluster with a local registry.

--- 06 - k8s files/Startup-script1.sh
+++ 06 - k8s files/Startup-script1.sh
@@ -1,15 +1,53 @@
-git clone https://github.com/albuild/snap.git
-cd snap
-bin/build
-bin/cp
-cd ~
+#install Kubernetes (Kind)
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
 
-sleep 10
+#install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-sudo snap install microk8s --classic --channel=1.33
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-sudo usermod -a -G microk8s $USER
-mkdir -p ~/.kube
-chmod 0700 ~/.kube
-su - $USER
 
+#set up a local registry
+docker run -d --restart=always -p 127.0.0.1:5001:5000 --network bridge --name kind-registry registry:2
+
+#create the cluster
+kind create cluster
+
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry]
+    config_path = "/etc/containerd/certs.d"
+EOF
+
+#Add the registry config to the nodes
+
+REGISTRY_DIR="/etc/containerd/certs.d/localhost:${reg_port}"
+for node in $(kind get nodes); do
+  docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
+  cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
+[host."http://${reg_name}:5000"]
+EOF
+done
+
+# Connect the registry to the cluster network if not already connected
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' kind-registry)" = 'null' ]; then
+  docker network connect "kind" kind-registry
+fi
+
+# Document the local registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
