e8c1675bc6ebbb8dd5c6df21e9300c91c5eb5b0a
Matt <matt@multicode.co.uk>
2025-11-21 11:40:36 +0000

    fix to startup script 1

--- 06 - k8s files/Startup-script1.sh
+++ 06 - k8s files/Startup-script1.sh
@@ -46,7 +46,7 @@ fi
 
 # install kubectl
 if ! command -v kubectl >/dev/null 2>&1; then
-  curl -LO "https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 fi
 
